b22dbee252444cf9f9f01646426bff64d1671f7d
TheSchlote <TheSchlote@gmail.com>
2020-03-23 01:58:30 -0500

    First Draft
    Having Trouble with Scriptable objects. Need to figure that out next.

---
+++ CardGame/Assets/BattleSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum Phases { START, PLAYERTURN, ENEMYTURN, WON, LOST}
+public class BattleSystem : MonoBehaviour
+{
+    public Phases state;
+
+    //public ScriptableObject slot1;
+    public GameObject cardSlot1;
+
+    public Image artworkImage;
+    public Text AtkText;
+    public Text HPText;
+
+    Card Slot1Card;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = Phases.START;
+        SetupBattle();
+    }
+    void SetupBattle()
+    {
+        //possibly a for loop for each of the card slots
+        GameObject Slot1GO = Instantiate(cardSlot1);
+        Slot1Card = Slot1GO.GetComponent<Card>();
+
+        cardSlot1.GetComponentInChildren<Image>(artworkImage);
+    }
+
+}
---
+++ CardGame/Assets/Scripts/Card.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Friends")]
+public class Card : ScriptableObject
+{
+    //Not sure about these\/
+    [SerializeField] string id;
+    public string ID { get { return id; } }
+    // /\
+
+    public string cardName;
+    public string description;
+    public Sprite artwork;
+
+    public int friendPoint;
+    public int attack;
+    public int health;
+
+    public void Print()
+    {
+        Debug.Log(cardName + ": " + description + "Card gives you this many Points!:" + friendPoint);
+    }
+
+    private void OnValidate()
+    {
+        string path = AssetDatabase.GetAssetPath(this);
+        id = AssetDatabase.AssetPathToGUID(path);
+
+    }
+
+}
+
+
