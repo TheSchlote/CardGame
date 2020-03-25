18015e95f9d36dc214541167e2076de2ff4b57f8
TheSchlote <TheSchlote@gmail.com>
2020-03-25 18:28:21 -0500

    Progress on Deck Builder
    I still need to find a way of selecting the cards .

---
+++ CardGame/Assets/Scripts/PopulateAvailableCards.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PopulateAvailableCards : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform grid;
+    
+
+    public List<Card> cards = new List<Card>();
+    //public int numberToCreate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Populate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Populate()
+    {
+        GameObject newObj;
+
+        foreach(Card card in cards)
+        {
+            newObj = (GameObject)Instantiate(prefab, grid);
+            newObj.GetComponent<CardDisplay>().card = card;
+        }
+    }
+
+    public void SelectCard()
+    {
+        //GameObject newObj;
+        Debug.Log(grid.childCount);
+    }
+
+}
